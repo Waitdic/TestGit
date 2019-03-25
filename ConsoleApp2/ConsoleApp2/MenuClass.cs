9e83988ba7eda730b4b675ad79e817c32809111b
Danis <waitdic161616@gmail.com>
2019-03-25 23:57:48 +0700

    setr

---
+++ ConsoleApp2/ConsoleApp2/MenuClass.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class MenuClass
+    {
+        public void Menu()
+        {
+            int menuPoint = 0;
+            Console.WriteLine("1) Constructor; \n0) Exit");
+            Console.Write("Point: ");
+            menuPoint = int.Parse(Console.ReadLine());
+            Console.WriteLine(menuPoint);
+            switch(menuPoint)
+            {
+                case 1:
+                    
+                        SampleConstructor sampCons = new SampleConstructor();
+                        sampCons.Car();
+                    break;
+                
+                default: Console.WriteLine("Its over!");
+                    break;
+            }
+        }
+    }
+}
---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Programm
+    {
+        static void Main(string[] args)
+        {
+            //int menuPoint;
+            Console.WriteLine("Please, enter menu number: ");
+            //menuPoint = Console.Read();
+            MenuClass M = new MenuClass();
+            M.Menu();
+            Console.ReadKey();
+        }
+    }
+}
