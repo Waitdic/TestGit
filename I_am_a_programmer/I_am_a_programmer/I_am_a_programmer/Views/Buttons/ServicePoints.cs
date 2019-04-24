10424794f6f16715fa54b0b56cf625c6ad5c3854
OLDEYSHINA <Stepa19991005@gmail.com>
2019-04-24 23:30:18 +0700

    классы

---
+++ I_am_a_programmer/I_am_a_programmer/I_am_a_programmer/Views/Buttons/ServicePoints.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Realms;
+
+namespace I_am_a_programmer.Views.Buttons
+{
+    public class Person : RealmObject
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Mail { get; set; }
+    }
+
+    public class GasStation :RealmObject
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string TimeWork { get; set; }
+    }
+}
+
+
